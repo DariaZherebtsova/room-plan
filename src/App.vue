23cc8bd5738f794b63e32daf0ddf08d12f900662
Daria Zherebtsova <dazhe2309@yandex.ru>
2021-11-15 19:08:30 +0300

    add plan

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <div class="room">
+      <Plan />
+      <Legend />
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import Plan from './components/Plan.vue';
+import Legend from './components/Legend.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Plan,
+    Legend,
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+
+.room {
+  display: grid;
+  grid-template-columns: 1fr 320px;
+  border: 1px solid #ccd8e4;
+  max-width: 1500px;
+  height: 100%;
+  background: white;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/Plan.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="plan"
+  >
+    <PlanSVG />
+  </div>
+</template>
+
+<script>
+import PlanSVG from '@/assets/images/plan.svg';
+
+export default {
+  components: {
+    PlanSVG,
+  },
+  data() {
+    return {
+    };
+  },
+
+}
+</script>
+
+<style>
+
+</style>
